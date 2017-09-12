01cf83067f8f9a2f0bb74ee013d4dcac1d4cfcbf
RoninhoMelo <roninhom95@gmail.com>
2017-09-12 17:20:05 -0300

    Add files via upload

--- funcoes.h
+++ funcoes.h
@@ -7,38 +7,48 @@
 #include <stdio.h>
 #include <conio.h>
 #include "structs.h"
-#include "struct.h"
+
 
 #define n_registo 9
 
 using namespace std;
 using namespace cadastros;
-using namespace cad;
+    void cadInstrutor(instrutor inst[], int *cont){
+            int i = 1;
+            int indinst;
+            char op;
+            char* arquivo = "instrutores.txt";
 
-void cadInstrutor(instrutor inst[], int *cont){
-        int i = 1;
-        char op;
+            do{
+                cout << "CADASTRANDO INSTRUTOR" << endl;
+                cout << "Nome:  ";
+                cin >> inst[i].nome;
+                cout << "Turno:  ";
+                cin >> inst[i].horario;
+                cout <<"Deseja cadastrar outro instrutor? ";
+                cin >> op;
 
-        do{
-            cout << "CADASTRANDO INSTRUTOR" << endl;
-            cout << "Nome:  ";
-            cin >> inst[i].nome;
-            cout << "Turno:  ";
-            cin >> inst[i].horario;
-            cout <<"Deseja cadastrar outro instrutor? ";
-            cin >> op;
-        char* arquivo = "instrutores.txt";
-        ofstream grava;
-        grava.open(arquivo, ios::app);
+            ofstream grava;
+            grava.open(arquivo, ios::app);
 
-        grava.seekp((i - 1) * sizeof(inst));
-        grava.write((const char *)(&i),sizeof(inst));
+            grava.seekp((i - 1) * sizeof(inst));
+            grava.write((const char *)(&i),sizeof(inst));
 
-        grava.close();
-        *cont = i;
-        i++;
-    }while((i<=9) && (op != 'n') && (op != 'N'));
+            grava.close();
+            *cont = i;
+            i++;
+        }while((i<=9) && (op != 'n') && (op != 'N'));
+     // verificar essa parte
+       ifstream abrir;
+       abrir.open(arquivo,ios::in);
+       abrir.read((char *)(&i),sizeof(inst));
+           while (abrir && ! abrir.eof()) {
+           cout<<inst[i].nome<< endl;
+           cout<<inst[i].horario<<endl;
+           abrir.read((char *)(&i),sizeof(inst));
+        }
+        abrir.close( );
+    }
 
-}
 
 #endif // FUNCOES_H_INCLUDED
